33d053226d10c2466ba059efecb8d5cfe9f960d5
mohamedamine456 <aminelachheb456@gmail.com>
2021-01-02 08:55:42 +0100

    databases on the image build..

--- images_files/mysql_image/install_script.sh
+++ images_files/mysql_image/install_script.sh
@@ -6,7 +6,7 @@
 #    By: mlachheb <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/12/22 14:22:06 by mlachheb          #+#    #+#              #
-#    Updated: 2021/01/01 09:01:54 by mlachheb         ###   ########.fr        #
+#    Updated: 2021/01/02 08:45:42 by mlachheb         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -25,3 +25,18 @@ apk add telegraf --repository http://dl-cdn.alpinelinux.org/alpine/edge/communit
 
 mv /mariadb-server.cnf /etc/my.cnf.d/mariadb-server.cnf;
 mv /telegraf.conf /etc/telegraf.conf;
+
+/etc/init.d/mariadb start;
+sleep 10;
+mysql -u root -e "CREATE USER admin IDENTIFIED BY 'password';";
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO admin IDENTIFIED BY 'password';";
+mysql -u root -e "CREATE DATABASE phpmyadmin;";
+mysql -u root -e "CREATE USER phpmyadmin IDENTIFIED BY 'password';";
+mysql -u root -e "GRANT ALL PRIVILEGES ON phpmyadmin.* TO phpmyadmin IDENTIFIED BY 'password';";
+mysql -u root -e "CREATE DATABASE wordpress;";
+mysql -u root -e "CREATE USER wordpress IDENTIFIED BY 'password';";
+mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO wordpress IDENTIFIED BY 'password';";
+mysql -u root -e "FLUSH PRIVILEGES;";
+mysql -u root < /wordpress.sql;
+mysql -u root < /create_tables.sql;
+/etc/init.d/mariadb stop;
