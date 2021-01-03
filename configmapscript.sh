cf95d67b866169b0dee189480ffe9a8c8654a86c
mohamedamine456 <aminelachheb456@gmail.com>
2021-01-03 18:18:27 +0100

    spending evening with ExteernalIP

--- configmapscript.sh
+++ configmapscript.sh
@@ -2,7 +2,7 @@
 kubectl get configmap kube-proxy -n kube-system -o yaml | \
 sed -e "s/strictARP: false/strictARP: true/" | \
 kubectl diff -f - -n kube-system
-
+echo "------------------hello every one there!------------------"
 # actually apply the changes, returns nonzero returncode on errors only
 kubectl get configmap kube-proxy -n kube-system -o yaml | \
 sed -e "s/strictARP: false/strictARP: true/" | \
